293884d1d75f22173352b88d04ce3d76b5f6508e
Yichen Chen <ychen3@stjude.org>
2025-09-24 11:18:03 -0500

    SQL testing: update R code for sql testing by using DSN

--- testing/test_sql.R
+++ testing/test_sql.R
@@ -1,20 +1,25 @@
 
+# Server name: DataWrangling2025
+# 
+# Database Name     | DSN    | Schema
+# ------------------------------------
+# datawrangling_R   | DW_R   | dbo  (dbo.test_sql)
+# datawrangling_SQL | DW_SQL | dbo
+# ccs_applications  | DW_CCS | dbo
+
+
 library(odbc)
 library(DBI)
 
-stjude_conn <- dbConnect(odbc(),
-                         Driver = "ODBC Driver 17 for SQL Server",
-                         Server = "DataWrangling2025",
-                         Database = "datawrangling_R",
-                         uid = "datawrangling2025",
-                         pwd = "l!XvQ8K$bvltOqwNUTq9qd2f0",
-                         #Port = "1433",
-                         #MARS_Connection="yes"
-                         )
+odbcListDataSources() # List out available DSN
+
+stjude_conn <- dbConnect(odbc(), dsn = "DW_R")     # connection to datawrangling_R
+# stjude_conn <- dbConnect(odbc(), dsn = "DW_SQL") # connection to datawrangling_SQL
+# stjude_conn <- dbConnect(odbc(), dsn = "DW_CCS") # connection to ccs_applications
 
 dbGetQuery(stjude_conn, "SELECT name FROM sys.databases;")
 
-# After running line 15, you should see the expected output as below.
+# After running line 20, you should see the expected output as below.
 # If you see errors or outputs other than the expected output, 
 # please email Yichen.chen@stjude.org with your screenshot.
 ####################################################################
@@ -29,3 +34,4 @@ dbGetQuery(stjude_conn, "SELECT name FROM sys.databases;")
 # 7  ccs_applications
 
 dbDisconnect(stjude_conn)
+
